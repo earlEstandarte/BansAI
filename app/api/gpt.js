3dd441fd132f1c55a313d79ffac367fe794daa05
earlEstandarte <earljohn.estandarte@foundationu.com>
2025-07-15 10:04:02 +0800

    fix: hid API key

--- app/api/gpt.js
+++ app/api/gpt.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const OPEN_AI_KEY = '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'
+const OPEN_AI_KEY = 'secret'
 const URL = 'https://api.openai.com/v1/chat/completions'
 
 export const sendMessageToGPT = async(messages) => {
