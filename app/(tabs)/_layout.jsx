20175d90997fffd4924465de5479fd1de79c18cb
earlEstandarte <earljohn.estandarte@foundationu.com>
2025-07-15 09:10:30 +0800

    Initial Commit

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -27,16 +27,37 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="fun"
         options={{
-          title: 'Explore',
+          title: 'Fun',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="assist"
+        options={{
+          title: 'Assist',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="learn"
+        options={{
+          title: 'Learn',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
---
+++ app/(tabs)/assist.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Assist = () => {
+  return (
+    <View>
+      <Text>Assist</Text>
+    </View>
+  )
+}
+
+export default Assist
+
+const styles = StyleSheet.create({})
